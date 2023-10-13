1d4bddbfe6a1121fcf0177024fa786fa4392a55a
Selaudin Agolli <selaudinagolli@Selaudins-MacBook-Pro.local>
2023-10-13 18:50:46 +0200

    Resize background picture

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -54,11 +54,11 @@ export const Navigation = (props) => {
                 Testimonials
               </a>
             </li> */}
-            <li>
+            {/* <li>
               <a href="#team" className="page-scroll">
                 Team
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#contact" className="page-scroll">
                 Contact
