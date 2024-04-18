fe6ad07914edcca667bbbbcf1a4d7b6b934a22ec
Selaudin <a.selaudin@gmail.com>
2024-04-18 17:50:28 +0200

    Update gitignore file, update email, remove form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -35,94 +35,36 @@ export const Contact = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
-            <div className="row">
+          <div className="row">
+            <div className="col-md-12">
               <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
+                <h2 >Get In Touch</h2>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
             </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
+          <div className="row justify-content-center">
+            <div className="contact-item col">
               <h3>Contact Info</h3>
+              {/* Address */}
               <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
+        <span>
+          <i className="fa fa-map-marker"></i> Address
+        </span>{" "}
                 {props.data ? props.data.address : "loading"}
               </p>
-            </div>
-            {/* <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
-            </div> */}
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
+              {/* Email */}
+              <div className="col">
+                <p>
+          <span>
+            <i className="fa fa-envelope-o"></i> Email
+          </span>{" "}
+                  {props.data ? props.data.email : "loading"}
+                </p>
+              </div>
             </div>
           </div>
-          <div className="col-md-12">
-            <div className="row">
+          <div className="row justify-content-center"> {/* Center horizontally */}
+            <div className="col-md-12">
               <div className="social">
                 <ul>
                   <li>
@@ -145,6 +87,7 @@ export const Contact = (props) => {
             </div>
           </div>
         </div>
+
       </div>
       <div id="footer">
         <div className="container text-center">
